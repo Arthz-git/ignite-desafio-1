7ac05bb685f598e15efdd0f233c19f9bfedbd3fa
Arthur <arthurcamara48@gmail.com>
2021-07-20 12:31:31 -0300

    Done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,38 +6,58 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+	const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+	function handleAddTask(newTaskTitle: string) {
+		//TODO - add new task
+		const newTask = {
+			id: new Date().getTime(),
+			title: newTaskTitle,
+			done: false
+		}
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+		setTasks(() => [...tasks, newTask])
+	}
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+	function handleToggleTaskDone(id: number) {
+		//TODO - toggle task done if exists
+		const updatedTasks = tasks.map(item => ({ ...item }))
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+		const unDoneTask = updatedTasks.find(item => item.id == id)
 
-      <TodoInput addTask={handleAddTask} />
+		if (unDoneTask) {
+			unDoneTask.done = !unDoneTask.done
+			setTasks(updatedTasks)
+		}
+	}
 
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
+	function handleRemoveTask(id: number) {
+		//TODO - remove task from state
+		setTasks(() => tasks.filter(item => item.id != id))
+	}
+
+	return (
+		<View style={styles.container}>
+			<Header
+				tasksCounter={tasks.length}
+			/>
+
+			<TodoInput
+				addTask={handleAddTask}
+			/>
+
+			<TasksList
+				tasks={tasks}
+				toggleTaskDone={handleToggleTaskDone}
+				removeTask={handleRemoveTask}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#EBEBEB'
+	}
 })
